dc5d82cfcd3c621fe26e312d2cca208edb447fce
Parsha Pourkhomami <parshap+git@gmail.com>
2011-10-07 02:44:44 -0700

    Improve number tests

--- test.js
+++ test.js
@@ -2,8 +2,22 @@ var MsgPack = require('coffeepack'),
     assert = require('assert');
 
 var object = {
+	numbers: [
+		Infinity,
+		-Infinity,
+		100, // positive fixnum
+		0xF0, // uint8
+		0xFFF0, //uint16
+		0xFFFFFFF0, // uint32
+		0xFFFFFFFFFFFFFF0, // uint64
+		-100, // negative fixnum
+		-0xF0, // int8
+		-0xFFF0, //int16
+		-0xFFFFFFF0, // int32
+		-0xFFFFFFFFFFFFFF0, // int64
+	],
+
     foo: "bar",
-    baz: [1, 2, 3, Infinity, -Infinity],
     person: {
         firstName: 'Devon',
         lastName: 'Govett'
